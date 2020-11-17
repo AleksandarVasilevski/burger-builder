f45f0c3e9e89d52c4798eb08dc388a8ff1212886
Aleksandar Vasilevski <vasilevski85@gmail.com>
2020-11-17 11:12:39 +0100

    added dynamic input elements

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,14 +4,61 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: ''
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Name'
+        },
+        value: ''
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street'
+        },
+        value: ''
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Zip Code'
+        },
+        value: ''
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country'
+        },
+        value: ''
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'E-Mail'
+        },
+        value: ''
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+              {value: 'fastest', displayValue: 'Fastest'},
+              {value: 'cheapest', displayValue: 'Cheapest'},
+            ]
+        },
+        value: ''
+      },
     },
     loading: false
   }
@@ -22,16 +69,6 @@ class ContactData extends Component {
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.totalPrice,
-      customer: {
-        name: 'Aleksandar Vasilevski',
-        adress: {
-          street: 'Radishanska Levo 152g',
-          zipCode: '1000',
-          country: 'Macedonia'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fast'
     }
     axios.post('/orders.json', order)
       .then(response => {
@@ -44,12 +81,22 @@ class ContactData extends Component {
   }
 
   render(){
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key],
+      });
+    }
     let form = (        
       <form>
-        <input className={classes.Input} type="text" name="name" placeholder="Name"/>
-        <input className={classes.Input} type="email" name="email" placeholder="Email"/>
-        <input className={classes.Input} type="text" name="street" placeholder="Street"/>
-        <input className={classes.Input} type="text" name="postal" placeholder="Postal Code"/>
+        {formElementsArray.map(formElement => (
+          <Input
+            key={formElement.id} 
+            elementType={formElement.config.elementType} 
+            elementConfig={formElement.config.elementConfig} 
+            value={formElement.config.value}/>
+        ))}
         <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
       </form>);
     if(this.state.loading) {
